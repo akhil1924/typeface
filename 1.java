d5d9afd49007d509ad76f46ed4b81692eb86878f
akhil1924 <61268761+akhil1924@users.noreply.github.com>
2022-11-21 16:43:55 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,8 +1,6 @@
 import java.util.*;
 public class ty {
-    public static void main(String a[]){ 
-Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+   public int base(int n){
         int x=0;
         String s1="",s2="";
         while(n>0){
@@ -12,6 +10,13 @@ Scanner sc=new Scanner(System.in);
             s2=s1+s2;
         }
         int i=Integer.parseInt(s2);
-        System.out.print(i);
+        return i;
+        }
+    public static void main(String a[]){ 
+Scanner sc=new Scanner(System.in);
+        
+        int n=sc.nextInt();
+        System.out.print(base(n));
+        
     }
 }
